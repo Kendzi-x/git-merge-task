b0fac5a1b3d8bb18294e8defb9467f6019673d91
Yuri Stepanov <Stepanoff.yura@ya.ru>
2019-04-19 18:40:56 +0300

    Update Calculator Tests

--- Calculator_Should.cs
+++ Calculator_Should.cs
@@ -59,19 +59,15 @@ namespace Kontur.Courses.Git
 		[TestCase("2", "asd", "3")]
 		public void ThreeArg_BadInput(params string[] args)
 		{
-			var calc = new Calculator();
-			calc.Calculate(new[] { "5" });
-			Assert.IsFalse(calc.Calculate(args).HasValue);
-			Assert.AreEqual(5.0, calc.Calculate(new string[] { }).Value);
+			Assert.IsFalse(Calc(string.Join("", args)).HasValue);
+			Assert.AreEqual(0, Calc("").Value);
 		}
 
 		[Test]
 		public void OneArg_BadInput()
 		{
-			var calc = new Calculator();
-			calc.Calculate(new[] { "5" });
-			Assert.IsFalse(calc.Calculate(new[] { "asd" }).HasValue);
-			Assert.AreEqual(5.0, calc.Calculate(new string[] { }).Value);
+			Assert.IsFalse(Calc("asd").HasValue);
+			Assert.AreEqual(0, Calc("").Value);
 		}
 
 	}
